8b6f7fd00e46f2d0977a43d323e76059c0451591
Slicepop <02kaden.seeger@gulllakecs.org>
2019-12-08 13:17:25 +0000

    Added the afk timing sequence then forgot to commit and added a way to make the sequence faster in the shop

--- Main.java
+++ Main.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 class Main {
-  
-  public static void cookie(){
-    Scanner sc = new Scanner(System.in);
+
+
     
-    double cookieclick = 0;
-    // cookieclick is how many cookies a in your bank
-    double cookiecount = 1;
-    // cookiecount is how many cookies per enter
-    while(true){
+  public static void cookie(){
+    try {
+      Scanner sc = new Scanner(System.in);
+
+      
+      int cookieclick = 0;
+      // cookieclick is how many cookies a in your bank
+      int cookiecount = 1;
+      // cookiecount is how many cookies you get per enter
+      int afksec = 1000;
+      // afksec is how much time it takes to finish the afk sequence
+      int upgrade = 0;
+      // upgrade is how the afk shop determines if you can buy the afk upgrade in the shop
+      int i = 0;
+      // i is to determine when afk sequence stops
+
+
+      while(true){
       String enter = sc.nextLine();
       if(enter.equals("quit")){
         System.exit(69);
@@ -16,10 +28,23 @@ class Main {
       else if(enter.equals("count")){
         System.out.println(cookiecount);
       }
+      else if(enter.equals("afk")){
+        System.out.println("How many secconds do you want to wait until the afk time is done (remember that you cannot stop the afk time until the secconds are up.)");
+        int afkwait = sc.nextInt();
+        while (i < afkwait) {
+            Thread.sleep(afksec);
+            System.out.println("Added +" + cookiecount + " Cookies\n");
+            cookieclick = cookieclick + cookiecount;
+            i++;
+        }
+        i = i - i;
+        System.out.println("Done!");
+      }
       else if(enter.equals("eat")){
         if(cookieclick < 1){
           System.out.println("You cannot eat right now you have no cookies");
         }
+        
         else{
           cookieclick = cookieclick - 1;
           System.out.println("You ate one cookie");
@@ -30,57 +55,120 @@ class Main {
         System.out.println("press enter to add a cookie and enter 'you can type a lot of commands like 'eat' to eat one cookie, 'shop' to enter the shop, and 'bal' to see how many cookies you have also type 'quit' to quit the game");
       }
       else if(enter.equals("debug")){
-        cookieclick = cookieclick + 1000;
-        cookiecount = cookiecount + 14;
-        System.out.println("ok added 1000 more cookies");
+        cookieclick = cookieclick + 10000 *10000000;
+        cookiecount = cookiecount + 10000*10000;
+        System.out.println("Welcome to developer mode.");
       }
       else if(enter.equals("shop")){
-        try {
-            
-            System.out.println("Checking out shop.");
-            Thread.sleep(1600);
-            System.out.println("Checking out shop..");
-            Thread.sleep(1500);
-            System.out.println("Checking out shop...");
-            System.out.println("");
+        System.out.println("\nChecking out shop.");
+        Thread.sleep(1200);
+        System.out.println("Checking out shop..");
+        Thread.sleep(1050);
+        System.out.println("Checking out shop...");
+        Thread.sleep(900);
+        System.out.println("\nYou can either upgrade the afk time or upgrade how many cookies you get per enter");
+        System.out.println("so what do you want to upgrade?\n\ntype 'cookies' if you want to upgrade your cookies and type 'afk' for upgrading afk\n");
+        String upgradeafk = sc.nextLine();
 
-            Thread.sleep(1000);
-         } catch (Exception e){
-           
-         }
-        
-        System.out.println("How many cookies do you want to make per enter \nremember that each extra cookie per enter is 100 cookies\n");
-        try{
-          Thread.sleep(1000);
-          System.out.println("How Many Cookeis Do You Want?");
-        }catch (Exception e){}
-        int number = sc.nextInt();
-         if(cookieclick > number * 100 ){
-           cookieclick = cookieclick - number * 100;
-           cookiecount = cookiecount + number; 
-           System.out.println("Ok you bought +" + number + " cookies per enter");
+        if(upgradeafk.equals("cookies")){
+          System.out.println("How many cookies do you want to make per enter \nremember that each extra cookie per enter is 100 cookies\n");
+          Thread.sleep(800);
+          System.out.println("How many cookies do you want?");
+          int number = sc.nextInt();
+          if(cookieclick > number * 100 ){
+          cookieclick = cookieclick - number * 100;
+          cookiecount = cookiecount + number; 
          } else{
-           System.out.println("you have not enough cookies to buy +" + number + " cookies per enter");
+           System.out.println("you have no cookies dude");
          }
+        } else if(upgradeafk.equals("afk")){
+          System.out.println("there are 4 upgrades the first three is 25% off the time\nthe last one is GOD mode where it is nearly instantaneously.\n\ntype 'upgrade' to buy the first one and then if you already bought the first or second the same command works for each upgrade.\n\n   1st upgrade:10,000,000,000 cookies\n   2nd upgrade:10,000,000,000,000,000,000 Cookies\n   3rd upgrade:10,000,000,000,000,000,000,000,000,000 Cookies\n   God upgrade:999,999,999,000,000,000,000,000,000,000,000,000,000,000 Cookies");
+          String afkupgrade = sc.nextLine();
+          if(afkupgrade.equals("upgrade")){
+            if(upgrade == 0 && cookieclick > 10000*1000000){
+              Thread.sleep(800);
+              System.out.println("Loading.");
+              Thread.sleep(800);
+              System.out.println("Loading..");
+              Thread.sleep(800);
+              System.out.println("Loading...");
+              Thread.sleep(800);
+              System.out.println("you bought the 1st upgrade.");
+              upgrade = upgrade + 1;
+              afksec = afksec - 250;
+              cookieclick = cookieclick - 10000 * 10000000;
+            }else if(upgrade == 0 && cookieclick < 10000 *10000000){
+              System.out.println("You dont have enough cookies to buy this");
+            }else if(upgrade == 1 && cookieclick > 10000 *10000000 * 10000000 * 10){
+              Thread.sleep(800);
+              System.out.println("Loading.");
+              Thread.sleep(800);
+              System.out.println("Loading..");
+              Thread.sleep(800);
+              System.out.println("Loading...");
+              Thread.sleep(800);
+              upgrade = upgrade + 1;
+              System.out.println("you bought the 2nd upgrade.");
+              afksec = afksec - 250;
+              cookieclick = cookieclick - 10000 *10000000 * 10000000 * 10;
+            }else if(upgrade == 1 && cookieclick < 10000 *10000000 * 10000000 * 10){
+              System.out.println("You dont have enough cookies to buy this");
+            }else if(upgrade ==  2 && cookieclick > 10000 *10000000 * 10000000 * 10 * 1000 * 1000 * 1000){
+              Thread.sleep(800);
+              System.out.println("Loading.");
+              Thread.sleep(800);
+              System.out.println("Loading..");
+              Thread.sleep(800);
+              System.out.println("Loading...");
+              Thread.sleep(800);
+              System.out.println("you bought the 3rd upgrade.");
+              upgrade = upgrade + 1;
+              afksec = afksec - 250;
+              cookieclick = cookieclick - 10000 *10000000 * 10000000 * 10 * 1000 * 1000 * 1000;
+            }else if(upgrade == 2 && cookieclick < 10000 *10000000 * 10000000 * 10 * 1000 * 1000 * 1000){
+              System.out.println("You dont have enough cookies to buy this");
+            }else if(upgrade == 3 && cookieclick > 999999999 * 1000000 * 1000000 * 1000000 * 1000000 * 1000000 * 1000){
+              Thread.sleep(800);
+              System.out.println("Loading.");
+              Thread.sleep(800);
+              System.out.println("Loading..");
+              Thread.sleep(800);
+              System.out.println("Loading...");
+              Thread.sleep(800);
+              System.out.println("you just bought GOD MODE enjoy!");
+              afksec = afksec - 225;
+              upgrade = upgrade + 1;
+              cookieclick = cookieclick - 999999999 * 1000000 * 1000000 * 1000000 * 1000000 * 1000000 * 1000;
+            }else if (upgrade == 3 && cookieclick < 999999999 * 1000000 * 1000000 * 1000000 * 1000000 * 1000000 * 1000){
+              System.out.println("You dont have enough cookies to buy this");
+            }
+          }else if(upgrade > 4){
+            System.out.println("You already have GOD MODE you basically just won the game there is no more upgrades.");
+        }else {System.out.println("try again by typing 'shop' and see if you misspelled something");}
+
+        }else{System.out.println("try again by typing 'shop' and see if you misspelled something");}
+        
          
          
        }
-      else if(enter.equals("bal")){
-        System.out.println(cookieclick + " Cookies");
-      }
-      else{
-        cookieclick = cookieclick + cookiecount;
-      }
+      else if(enter.equals("bal")){System.out.println("you have " + cookieclick + " Cookies");}
+      else{System.out.println("Added +" + cookiecount + " cookies");
+        cookieclick = cookieclick + cookiecount;}
+    }
+        
+    } catch (InterruptedException e) {
+        e.printStackTrace();
     }
   }
-  public static void main(String[] args) {
-    System.out.println("\n");
-        System.out.println("---------------------------------------------------------------");
-    System.out.println("Welcome to my version of cookie clicker press enter to add a cookie type 'help' for a list of all of the commands");
   
-   System.out.println("---------------------------------------------------------------");
-   System.out.println("\n");
+  
+  
+    public static void main(String[] args){
+    
+    System.out.println("\n==================================================================================================================");
+    System.out.println("Welcome to my version of cookie clicker press enter to add a cookie type 'help' for a list of all of the commands");
+   System.out.println("==================================================================================================================\n");
+   cookie();
+  }
 
-    cookie();
-  }      
-}
+  }
